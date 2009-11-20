6e41fd45f1297d818d51b888d99258c59c10ad39
SND\ferrero_cp <SND\ferrero_cp@142e9c35-602f-4880-9f37-ec48a24daa6a>
2009-11-20 20:46:32 +0000

    git-svn-id: https://xrealengine.svn.codeplex.com/svn@41582 142e9c35-602f-4880-9f37-ec48a24daa6a

---
+++ XRealEngine/XRealEngine.ContentPipeline/SpritesSheetContent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework.Content;
+
+namespace XRealEngine.ContentPipeline
+{
+    class SpritesSheetContent
+    {
+        [ContentSerializerRuntimeType("XRealEngine.Sprites.SpritesSheet, XRealEngine")]
+        public class SpritesSheetContent
+        {
+            ////////////////////////////////////////////////////////////////////////////////////////////////////
+            /// <summary>List that holds the sprites definitions contained in the SpritesSheet</summary>
+            ////////////////////////////////////////////////////////////////////////////////////////////////////
+            [ContentSerializer]
+            public List<SpriteDefinition> spritesList;
+
+            ////////////////////////////////////////////////////////////////////////////////////////////////////
+            /// <summary>Texture that holds the different sprites graphics</summary>
+            ////////////////////////////////////////////////////////////////////////////////////////////////////
+            [ContentSerializerIgnore]
+            public Texture2DContent texture;
+        }
+    }
+}

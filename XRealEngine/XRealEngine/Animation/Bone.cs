cd0b4d59919174db6c2a749430c442035182e1df
SND\ferrero_cp <SND\ferrero_cp@142e9c35-602f-4880-9f37-ec48a24daa6a>
2011-05-10 19:09:40 +0000

    Creation of the basic bones system
    git-svn-id: https://xrealengine.svn.codeplex.com/svn@60666 142e9c35-602f-4880-9f37-ec48a24daa6a

---
+++ XRealEngine/XRealEngine/Animation/Bone.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace XRealEngine.Animation
+{
+    public sealed class Bone
+    {
+        public Bone Parent { get; internal set; }
+        public BonesCollection Childs { get; private set; }
+        public float Angle { get;set; }
+        public string Name { get; set; }
+        public float Length { get; set; }
+
+        public Bone(string name, float length, float angle):this()
+        {
+            Name = name;
+            Length = length;
+            Angle = angle;
+        }
+
+        public Bone()
+        {
+            Childs = new BonesCollection(this);
+            Parent = null;
+        }
+    }
+}
---
+++ XRealEngine/XRealEngine/Animation/BonesCollection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XRealEngine.Animation
+{
+    public sealed class BonesCollection
+    {
+        List<Bone> _bones;
+        Bone _parent;
+
+        private BonesCollection()
+        {
+        }
+
+        internal BonesCollection(Bone bone)
+        {
+            _parent = bone;
+            _bones = new List<Bone>();
+        }
+
+        public void Add(Bone bone)
+        {
+            bone.Parent = _parent;
+            _bones.Add(bone);
+        }
+    }
+}
